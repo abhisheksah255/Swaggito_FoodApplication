adf3dfe244c15dd4c6941a59381a4c5972555947
abhisheksah255 <abhisheksah255@gmail.com>
2025-10-09 10:07:02 +0000

    Adding selfhosted runner and Trivy scan

--- Terraform/terraform-backend/main.tf
+++ Terraform/terraform-backend/main.tf
@@ -11,10 +11,10 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-resource "aws_s3_bucket_acl" "terraform_state_acl" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "terraform_state_acl" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
